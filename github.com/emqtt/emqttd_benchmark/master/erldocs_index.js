f099f5acd5631c5da3d0f312a679e343c71fd967
Travis CI <travis@atdot.dot>
2015-05-29 17:45:11 +0000

    Successfully applied Pull Request \#48, please close PR & delete branch

---
+++ github.com/emqtt/emqttd_benchmark/master/erldocs_index.js
@@ -0,0 +1,1 @@
+var index = [['app','emqttd_benchmark','emqttd_benchmark','[application]'],['mod','emqttd_benchmark','emqttd_benchmark','   eMQTTD Benchmark Client.'],['fun','emqttd_benchmark','emqttd_benchmark:connect/3',[]],['fun','emqttd_benchmark','emqttd_benchmark:loop/3',[]],['fun','emqttd_benchmark','emqttd_benchmark:run/3',[]],['fun','emqttd_benchmark','emqttd_benchmark:start/1',[]]];
