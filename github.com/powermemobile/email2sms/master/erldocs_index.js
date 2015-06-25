5d37724f12885f995f3a5af57d3d111273ae3107
Pierre Fenoll <pierrefenoll@gmail.com>
2015-06-26 01:20:05 +0200

    daily update

--- github.com/powermemobile/email2sms/master/erldocs_index.js
+++ github.com/powermemobile/email2sms/master/erldocs_index.js
@@ -1,1 +1,1 @@
-var index = [['app','email2sms','email2sms','[application]'],['mod','email2sms','email2sms',[]],['mod','email2sms','email2sms_develop',[]],['fun','email2sms','email2sms:main/1',[]],['fun','email2sms','email2sms_develop:init/0',[]]];
+var index = [['app','email2sms','email2sms','[application]'],['mod','email2sms','email2sms_develop',[]],['mod','email2sms','email2sms_errors',[]],['fun','email2sms','email2sms_develop:init/0',[]],['fun','email2sms','email2sms_errors:format_error/1',[]]];
